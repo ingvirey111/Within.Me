6e863758f0a0cafa43e02404abcac26d2c3e3901
ingvirey111 <ingvirey@gmail.com>
2025-11-11 11:49:41 -0800

    Initialize WithinMe project with base SwiftUI setup

---
+++ WithinMe/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Home Page")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
